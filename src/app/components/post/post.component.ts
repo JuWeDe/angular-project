65857fe0a6d4149868264ecf6d5850b7fcd8826d
JuWeDe <RomaN18011998@gmail.com>
2023-05-15 00:55:38 +0300

    edited post.component.ts

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,30 +1,28 @@
 import { Component, Input } from '@angular/core';
-import {RecordService} from "../../record-service.service";
-import {Post} from "../../app.component";
+import { RecordService } from '../../record-service.service';
+import { Post } from '../../app.component';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
 })
 export class PostComponent {
-  @Input()id:any;
-  @Input()author: any;
-  @Input()title: any;
-  @Input()category: any;
-  @Input()content: any;
+  @Input() id?: any;
+  @Input() author?: string;
+  @Input() title?: string;
+  @Input() category?: string;
+  @Input() content?: string;
 
-  records: Post[]=[];
+  records: Post[] = [];
 
   constructor(private recordService: RecordService) {
-    this.records=recordService.records
+    this.records = recordService.records;
   }
 
-  deletePost(id: number)
-  {
+  deletePost(id: number) {
     this.recordService.deletePost(id);
   }
-  toArchieve(id: number)
-  {
+  toArchieve(id: number) {
     this.recordService.toArcheve(id);
   }
 }
